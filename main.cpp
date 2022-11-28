ef2638bd330f1b11091cb61c37bc41576271a10f
Roman Razvan <98484095+razvanrmn@users.noreply.github.com>
2022-11-28 16:07:23 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include "AplicatieBanca.h"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
+	
+	
+	//vector <ContBancar*> conturi = { ("1", 500, "EUR") , ("2", 1000, "RON") };
+	//vector <Client*> clienti = { ("Rmn", "Rzv", "Cvn", 2, &conturi), ("Cqv", "Dah", "Fuq", 2, &conturi) };
+
+	//ContBancar* conturii = { ("Daa", 500, "EUR") , ("Nuu", 1000, "RON") };
+	//Client* clientii = { ("Rmn", "Rzv", "Cvn", 2, conturii), ("Cqv", "Dah", "Fuq", 2, conturii) };
+	//Banca banc("BCR", 2, clienti);
+
+	ContBancar conturi[] = {
+		("Cont1",669.69,"EUR"),
+		("Cont2", 1543,34, "RON")
+	};
+
+
+
+
 
 }
