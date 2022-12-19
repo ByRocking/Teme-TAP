9ffa2c5059c9b02954431120ebf4a7b4ec11ef2a
Roman Razvan <98484095+razvanrmn@users.noreply.github.com>
2022-12-19 12:26:24 +0200

    Add files via upload

--- lab4Ex1/Main.cpp
+++ lab4Ex1/Main.cpp
@@ -1,16 +1,37 @@
-#include "Matrice.h"
+#include "Fractie.h"
 #include <iostream>
 
 using namespace std;
 
 void main() {
-	Matrice a(2,2);
-	Matrice b(2,2);
-	
-	a.operator>>(cin);
-	b.operator>>(cin);
-	cout << endl;
-	(a + b).operator<<(cout) << endl;
-	(a - b).operator<<(cout) << endl;
-	(a * b).operator<<(cout) << endl;
+    int numitor1, numarator1, numitor2, numarator2;
+
+    cout << "Introduceti prima fractie : " << endl;
+    cin >> numarator1 >> numitor1;
+    Fractie f1 = Fractie(numarator1, numitor1);
+
+    cout << "Introduceti a doua fractie " << endl;
+    cin >> numarator2 >> numitor2;
+    Fractie f2 = Fractie(numarator2, numitor2);
+
+    cout << endl;
+    cout << "Fractiile sunt: " << numarator1 << "/" << numitor1 << " si " << numarator2 << "/" << numitor2 << endl;
+
+    cout << "Test operatii : " << endl;
+    cout << numarator1 << "/" << numitor1 << " + " << numarator2 << "/" << numitor2 << " = " << (f1 + f2).getNumarator() << "/" << (f1 + f2).getNumitor() << endl;
+    cout << numarator1 << "/" << numitor1 << " - " << numarator2 << "/" << numitor2 << " = " << (f1 - f2).getNumarator() << "/" << (f1 - f2).getNumitor() << endl;
+    cout << numarator1 << "/" << numitor1 << " * " << numarator2 << "/" << numitor2 << " = " << (f1 * f2).getNumarator() << "/" << (f1 * f2).getNumitor() << endl;
+    cout << numarator1 << "/" << numitor1 << " / " << numarator2 << "/" << numitor2 << " = " << (f1 / f2).getNumarator() << "/" << (f1 / f2).getNumitor() << endl;
+
+    cout << numarator1 << "/" << numitor1 << " += " << numarator2 << "/" << numitor2 << " = " << (f1 += f2).getNumarator() << "/" << (f1 += f2).getNumitor() << endl;
+    cout << numarator1 << "/" << numitor1 << " -= " << numarator2 << "/" << numitor2 << " = " << (f1 -= f2).getNumarator() << "/" << (f1 -= f2).getNumitor() << endl;
+    cout << numarator1 << "/" << numitor1 << " *= " << numarator2 << "/" << numitor2 << " = " << (f1 *= f2).getNumarator() << "/" << (f1 *= f2).getNumitor() << endl;
+    cout << numarator1 << "/" << numitor1 << " /= " << numarator2 << "/" << numitor2 << " = " << (f1 /= f2).getNumarator() << "/" << (f1 /= f2).getNumitor() << endl;
+
+    cout << numarator1 << "/" << numitor1 << " < " << numarator2 << "/" << numitor2 << " " << boolalpha << (f1 < f2) << endl;
+    cout << numarator1 << "/" << numitor1 << " <= " << numarator2 << "/" << numitor2 << " " << boolalpha << (f1 <= f2) << endl;
+    cout << numarator1 << "/" << numitor1 << " > " << numarator2 << "/" << numitor2 << " " << boolalpha << (f1 > f2) << endl;
+    cout << numarator1 << "/" << numitor1 << " >= " << numarator2 << "/" << numitor2 << " " << boolalpha << (f1 >= f2) << endl;
+    cout << numarator1 << "/" << numitor1 << " == " << numarator2 << "/" << numitor2 << " " << boolalpha << (f1 == f2) << endl;
+    cout << numarator1 << "/" << numitor1 << " != " << numarator2 << "/" << numitor2 << " " << boolalpha << (f1 != f2) << endl;
 }
